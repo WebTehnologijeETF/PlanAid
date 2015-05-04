dcf28d55f8a5456a8c17a475c3126fc49de88499
zloco <aploco1@etf.unsa.ba>
2015-05-04 12:20:35 +0200

    SPA preko innerhtml

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,3 +1,17 @@
+function PrikaziVijesti(loc) {
+	var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        	//document.open();
+            //document.write(xmlhttp.responseText);
+            //document.close();
+            document.getElementById("frejm").innerHTML = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open('GET', loc, true);
+    xmlhttp.send();
+}
+
 function PrikaziNaslovnicu() {
 	document.getElementById("submenu_lokacije").className="submenu_invisible";
 	document.getElementById("submenu_prijava").className="submenu_invisible";
@@ -37,16 +51,25 @@ function PrikaziStranicu(stranica) {
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        	document.open();
-            document.write(xmlhttp.responseText);
-            document.close();
-            //document.getElementById("glavni").innerHTML = xmlhttp.responseText;
+        	//document.open();
+            //document.write(xmlhttp.responseText);
+            //document.close();
+            document.getElementById("glavni").innerHTML = xmlhttp.responseText;
         }
     };
 
-    //xmlhttp.open('GET', stranica, true);
-    xmlhttp.open('GET', 'http://localhost/PlanAid/' + stranica + '.html', true);
+    xmlhttp.open('GET', stranica + '.html', true);
+    //xmlhttp.open('GET', 'http://localhost/PlanAid/' + stranica + '.html', true);
     xmlhttp.send();
+    if(stranica === 'naslovnica_novevijesti') {
+    	PrikaziVijesti('nove_vijesti.html');
+    }
+    else if(stranica === 'naslovnica_svevijesti') {
+    	PrikaziVijesti('sve_vijesti.html');
+    }
+    else if(stranica === 'naslovnica_najcitanije') {
+    	PrikaziVijesti('najcitanije_vijesti.html');
+    } 
 }
 
 function Izmijeni() {
